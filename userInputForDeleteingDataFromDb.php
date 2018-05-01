58fc80c1d7753744910794402c2a3839e160c3a1
Himanshi <32532525+himanshi-sharmaa@users.noreply.github.com>
2018-05-02 02:00:24 +0530

    Add files via upload

---
+++ userInputForDeleteingDataFromDb.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>User input</title></head>
+<body>
+   <body>
+    <h1>Delete details:</h1>
+       <form method="post" action="deletedatafromdb.php">
+    Reg No:
+       <input type="text" name="reg">
+       <input type="submit" name="submit" value="submit">
+       </form>
+        </body> 
+</body>
+</html>
